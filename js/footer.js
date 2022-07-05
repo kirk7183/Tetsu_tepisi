3e582c6724488b52b348bbe510ff1fc6233666eb
vasicigor <vasicigorjsp@gmail.com>
2022-07-05 14:07:17 +0200

    pocetna, komentari, o nama - u izradi

---
+++ js/footer.js
@@ -0,0 +1,68 @@
+var footer = `<footer>
+<div class="wrapper-footer">
+    <div class="time">
+        <div class="working-time">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/Asset 22-8.png" alt="pescani sat">
+            </div>
+            <div class="time-info">
+                <p class="text-color">RADNO VREME</p>
+                <p class="text-color">Ponedeljak - Petak</p>
+                <p>9-19h</p>
+                <p class="text-color">Subota</p>
+                <p>9-16h</p>
+            </div>
+        </div>
+    </div>
+    <div class="contact">
+        <div class="phone">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/Asset 22-8.png" alt="telefon">
+            </div>
+            <div class="tel-info">
+                <p class="text-color">TELEFON</p>
+                <p>032 727 337</p>
+            </div>
+        </div>
+        <div class="address">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/Asset 22-8.png" alt="adresa">
+            </div>
+            <div class="address-info">
+                <p class="text-color">ADRESA</p>
+                <p>Rudnička 6,</p>
+                <p>32300 Gornji Milanovac</p>
+            </div>
+        </div>
+    </div>
+    <div class="social-email">
+        <div class="facebook">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/Asset 23-8.png" alt="facebook">
+            </div>
+            <div class="fb-info text-color">
+                <p>Tetsu Tepisi</p>
+                <P>Carpets & Flooring Store</P>
+            </div>
+        </div>
+        <div class="instagram">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/Asset 24-8.png" alt="instagram">
+            </div>
+            <div class="instagram-info text-color">
+                <p>Tetsu_tepisi</p>
+            </div>
+        </div>
+        <div class="email">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/Asset 25-8.png" alt="instagram">
+            </div>
+            <div class="email-info text-color">
+                <p>Tetsutepisi@gmail.com</p>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>`;
+
+document.getElementById('footerHTML').innerHTML = footer;
--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
 /* navbar for every page */
-var navBar = `<section class="top-nav">
+var navBar = `<nav class="top-nav">
 <div class="img-wrapper">
     <a href="/">
         <img src="/assets/images/T-logo-trans.png" alt="Tetsu logo transparent">
@@ -18,18 +18,18 @@ var navBar = `<section class="top-nav">
             <a href="/proizvodi.html">Proizvodi </a>
         </li>
         <li>
-            <a href="/galerija.html">Galerija </a>
+            <a href="/odrzavanje.html">Održavanje </a>
         </li>
         <li>
             <a href="/o_nama.html">O nama </a>
         </li>
         <li>
-            <a href="/komentari.html">Komentari </a>
+            <a href="https://www.google.com/maps/place/Tetsu+Tepisi/@44.0423319,20.4833787,17z/data=!4m7!3m6!1s0x4757411df92331b7:0x12c152c269f34a08!8m2!3d44.0423281!4d20.4855728!9m1!1b1" target="_blank">Komentari </a>
         </li>
 
     </ul>
 </div>
-</section>`;
+</nav>`;
 
 document.getElementById('navbarHTML').innerHTML = navBar;
 
