c0e70aa08a496a6688682ad94149a8c0e1a84873
vasicigor <vasicigorjsp@gmail.com>
2022-07-11 12:43:10 +0200

    little changes and testing

--- js/footer.js
+++ js/footer.js
@@ -8,59 +8,65 @@ var footer = `<footer>
             <div class="time-info">
                 <p class="text-color">RADNO VREME</p>
                 <p class="text-color">Ponedeljak - Petak</p>
-                <p>9-19h</p>
+                <p class="text-color2">9-19h</p>
                 <p class="text-color">Subota</p>
-                <p>9-16h</p>
+                <p class="text-color2">9-16h</p>
             </div>
         </div>
     </div>
     <div class="contact">
         <div class="phone">
             <div class="leftIcon">
-                <img src="/assets/images/ornaments/Asset 22-8.png" alt="telefon">
+                <img src="/assets/images/ornaments/phone.png" alt="telefon">
             </div>
             <div class="tel-info">
                 <p class="text-color">TELEFON</p>
-                <p>032 727 337</p>
+                <p class="text-color2">032 727 337</p>
             </div>
         </div>
         <div class="address">
             <div class="leftIcon">
-                <img src="/assets/images/ornaments/Asset 22-8.png" alt="adresa">
+                <img src="/assets/images/ornaments/address.png" alt="adresa">
             </div>
             <div class="address-info">
                 <p class="text-color">ADRESA</p>
-                <p>Rudnička 6,</p>
-                <p>32300 Gornji Milanovac</p>
+                <p class="text-color2">Rudnička 6,</p>
+                <p class="text-color2">32300 Gornji Milanovac</p>
             </div>
         </div>
     </div>
     <div class="social-email">
-        <div class="facebook">
-            <div class="leftIcon">
-                <img src="/assets/images/ornaments/Asset 23-8.png" alt="facebook">
+        <a href="https://www.facebook.com/Tetsu032" target="_blank">
+            <div class="facebook">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/Asset 23-8.png" alt="facebook">
+                </div>
+                <div class="fb-info text-color">
+                    <p>Tetsu Tepisi</p>
+                    <P>Carpets & Flooring Store</P>
+                </div>
             </div>
-            <div class="fb-info text-color">
-                <p>Tetsu Tepisi</p>
-                <P>Carpets & Flooring Store</P>
+        </a>
+        <a href="https://www.instagram.com/tetsu_tepisi/?hl=en" target="_blank">
+            <div class="instagram">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/instagram.png" alt="instagram">
+                </div>
+                <div class="instagram-info text-color">
+                    <p>Tetsu_tepisi</p>
+                </div>
             </div>
-        </div>
-        <div class="instagram">
-            <div class="leftIcon">
-                <img src="/assets/images/ornaments/Asset 24-8.png" alt="instagram">
+        </a>
+        <a href="mailto:tetsutepisi@gmail.com" target="_blank">
+            <div class="email">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/Asset 25-8.png" alt="instagram">
+                </div>
+                <div class="email-info text-color">
+                    <p>Tetsutepisi@gmail.com</p>
+                </div>
             </div>
-            <div class="instagram-info text-color">
-                <p>Tetsu_tepisi</p>
-            </div>
-        </div>
-        <div class="email">
-            <div class="leftIcon">
-                <img src="/assets/images/ornaments/Asset 25-8.png" alt="instagram">
-            </div>
-            <div class="email-info text-color">
-                <p>Tetsutepisi@gmail.com</p>
-            </div>
-        </div>
+        </a>
     </div>
 </div>
 </footer>`;
