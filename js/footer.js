95efd8f5c85af298e2c9632bad710b36e5403815
vasicigor <vasicigorjsp@gmail.com>
2022-07-25 11:18:28 +0200

    next and previus on images fixed, some little changes in code

---
+++ js/footer.js
@@ -0,0 +1,73 @@
+var footer = `<footer>
+<div class="wrapper-footer">
+    <div class="time">
+        <div class="working-time">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/hourglass.svg" alt="pescani sat">
+            </div>
+            <div class="time-info">
+                <p class="text-color">RADNO VREME</p>
+                <p class="text-color">Ponedeljak - Petak</p>
+                <p class="text-color2">9-19h</p>
+                <p class="text-color">Subota</p>
+                <p class="text-color2">9-16h</p>
+            </div>
+        </div>
+    </div>
+    <div class="contact">
+        <div class="phone">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/phone.svg" alt="telefon">
+            </div>
+            <div class="tel-info">
+                <p class="text-color">TELEFON</p>
+                <p class="text-color2">032 727 337</p>
+            </div>
+        </div>
+        <div class="address">
+            <div class="leftIcon">
+                <img src="/assets/images/ornaments/location.svg" alt="adresa">
+            </div>
+            <div class="address-info">
+                <p class="text-color">ADRESA</p>
+                <p class="text-color2">Rudnička 6,</p>
+                <p class="text-color2">32300 Gornji Milanovac</p>
+            </div>
+        </div>
+    </div>
+    <div class="social-email">
+        <a href="https://www.facebook.com/Tetsu032" target="_blank">
+            <div class="facebook">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/facebook.svg" alt="facebook">
+                </div>
+                <div class="fb-info text-color">
+                    <p>Tetsu Tepisi</p>
+                </div>
+            </div>
+        </a>
+        <a href="https://www.instagram.com/tetsu_tepisi/?hl=en" target="_blank">
+            <div class="instagram">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/instagram.svg" alt="instagram">
+                </div>
+                <div class="instagram-info text-color">
+                    <p>Tetsu_tepisi</p>
+                </div>
+            </div>
+        </a>
+        <a href="mailto:tetsutepisi@gmail.com" target="_blank">
+            <div class="email">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/email.svg" alt="email">
+                </div>
+                <div class="email-info text-color">
+                    <p>Tetsutepisi@gmail.com</p>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
+</footer>`;
+
+document.getElementById('footerHTML').innerHTML = footer;
---
+++ js/proizvodi.js
@@ -0,0 +1,45 @@
+//1
+var anchorTag1 = document.getElementsByClassName('d-flex')[0];
+var imageClick1 = document.getElementsByClassName('banner-1')[0].getElementsByClassName('right-img')[0].getElementsByTagName('img')[0];
+
+anchorTag1.onclick = function(event) {
+    event.preventDefault();
+    imageClick1.click();
+}
+
+
+//2
+var anchorTag2 = document.getElementsByClassName('d-flex')[1];
+var imageClick2 = document.getElementsByClassName('banner-2')[0].getElementsByClassName('right-img')[0].getElementsByTagName('img')[0];
+
+anchorTag2.onclick = function(event) {
+    event.preventDefault();
+    imageClick2.click();
+}
+
+//3
+var anchorTag3 = document.getElementsByClassName('d-flex')[2];
+var imageClick3 = document.getElementsByClassName('banner-3')[0].getElementsByClassName('right-img')[0].getElementsByTagName('img')[0];
+
+anchorTag3.onclick = function(event) {
+    event.preventDefault();
+    imageClick3.click();
+}
+
+//4
+var anchorTag4 = document.getElementsByClassName('d-flex')[3];
+var imageClick4 = document.getElementsByClassName('banner-4')[0].getElementsByClassName('right-img')[0].getElementsByTagName('img')[0];
+
+anchorTag4.onclick = function(event) {
+    event.preventDefault();
+    imageClick4.click();
+}
+
+//5
+var anchorTag5 = document.getElementsByClassName('d-flex')[4];
+var imageClick5 = document.getElementsByClassName('banner-5')[0].getElementsByClassName('right-img')[0].getElementsByTagName('img')[0];
+
+anchorTag5.onclick = function(event) {
+    event.preventDefault();
+    imageClick5.click();
+}
