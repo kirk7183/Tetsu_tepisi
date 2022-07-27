f22d0843f669b1dd89fb5f8a81374bce43314a13
vasicigor <vasicigorjsp@gmail.com>
2022-07-27 13:15:59 +0200

    added Google maps, calls on click

--- public/js/footer.js
+++ public/js/footer.js
@@ -15,25 +15,29 @@ var footer = `<footer>
         </div>
     </div>
     <div class="contact">
-        <div class="phone">
-            <div class="leftIcon">
-                <img src="/assets/images/ornaments/phone.svg" alt="telefon">
+        <a href="tel:032727337">
+            <div class="phone">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/phone.svg" alt="telefon">
+                </div>
+                <div class="tel-info">
+                    <p class="text-color">TELEFON</p>
+                    <p class="text-color2">032 727 337</p>
+                </div>
             </div>
-            <div class="tel-info">
-                <p class="text-color">TELEFON</p>
-                <p class="text-color2">032 727 337</p>
+        </a>
+        <a href="https://www.google.com/maps/place/Tetsu+Tepisi/@44.0423176,20.4834236,17z/data=!4m13!1m7!3m6!1s0x475741ce28211d8d:0x4cfbdb9833ee7532!2sRudni%C4%8Dka+6,+Gornji+Milanovac+32300!3b1!8m2!3d44.0423176!4d20.4856123!3m4!1s0x4757411df92331b7:0x12c152c269f34a08!8m2!3d44.0423281!4d20.4855728">
+            <div class="address">
+                <div class="leftIcon">
+                    <img src="/assets/images/ornaments/location.svg" alt="adresa">
+                </div>
+                <div class="address-info">
+                    <p class="text-color">ADRESA</p>
+                    <p class="text-color2">Rudnička 6,</p>
+                    <p class="text-color2">32300 Gornji Milanovac</p>
+                </div>
             </div>
-        </div>
-        <div class="address">
-            <div class="leftIcon">
-                <img src="/assets/images/ornaments/location.svg" alt="adresa">
-            </div>
-            <div class="address-info">
-                <p class="text-color">ADRESA</p>
-                <p class="text-color2">Rudnička 6,</p>
-                <p class="text-color2">32300 Gornji Milanovac</p>
-            </div>
-        </div>
+        </a>
     </div>
     <div class="social-email">
         <a href="https://www.facebook.com/Tetsu032" target="_blank">
